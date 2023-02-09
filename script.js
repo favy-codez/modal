c3e6885ded0b3426ceacf67201a6ac267263defe
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-09 06:16:16 +0100

    selected elements for DOM manipulation

---
+++ script.js
@@ -0,0 +1,5 @@
+'use script';
+
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay hidden');
