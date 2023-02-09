2637df04e5c504ab87e28020a8cf6a82670308b7
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-09 06:50:40 +0100

    created a for function that opens up the modal

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay hidden');
 const openModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < openModal.length; i++) {
+    // add event listener on the 3 buttons
+   openModal[i].addEventListener('click', function(){
+    console.log('btn clicked');
+    // so the hidden class is set to display-none, we want to use classlist to remove it
+    modal.classList.remove('hidden');
+    });
+}
