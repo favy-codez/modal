f593a3211249070e4f3242dcbe479fad85cc47e4
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-02-09 06:59:03 +0100

    fixed a bug

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
-const overlay = document.querySelector('.overlay hidden');
+const overlay = document.querySelector('.overlay');
 const openModal = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < openModal.length; i++) {
@@ -11,5 +11,6 @@ for (let i = 0; i < openModal.length; i++) {
     console.log('btn clicked');
     // so the hidden class is set to display-none, we want to use classlist to remove it
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
     });
 }
